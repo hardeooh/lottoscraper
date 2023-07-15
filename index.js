023a9bc1d27bf1d9161c408eed4a70e767f7d60c
hardeooh <harrydaleoh@gmail.com>
2023-07-15 03:02:02 -0700

    unique hrefs from lottery site pulled

--- index.js
+++ index.js
@@ -6,14 +6,18 @@
 
         const page = await browser.newPage();
         await page.goto('https://www.calottery.com/scratchers#endTable');
-        	
+        await page.waitForTimeout(3000)	
+
+
         const hrefs = await page.evaluate(() => {
           return Array.from(document.links).map(item => item.href).filter(item => item.indexOf('$') > -1);
           });
         
         hrefs.sort()
+        const unique_hrefs = hrefs.filter((e,i,a)=> a.indexOf(e) === i)
 
-        console.log(hrefs, hrefs.length)
+        console.log(unique_hrefs, unique_hrefs.length)
+        console.log(hrefs.indexOf('https://www.calottery.com/scratchers/$1/ladybug-bucks-1573'))
         console.log("hello world")
         await browser.close();
     }
