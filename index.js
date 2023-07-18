f2ff7aa7d6a1fc13c3937e91641e7b32cd508222
hardeooh <harrydaleoh@gmail.com>
2023-07-17 20:58:59 -0700

    added express

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const playwright = require('playwright');
 require('dotenv').config()
 const mysql = require('mysql2')
+const express = require('express')
 const connection = mysql.createConnection(process.env.DATABASE_URL)
 console.log('Connected to PlanetScale!')
 connection.end()
+
+const app = express();
+const port = 3000;
+
 
 const { getURL, extractData } = require('./pullData.js')
 
