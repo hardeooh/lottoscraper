5397f16c1a2ef4d8deef49717f1c2ad6c6b6679f
hardeooh <harrydaleoh@gmail.com>
2023-07-24 19:54:28 -0700

    return cleanScratcherData results as an object complete

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ app.get('/api/gt', (req,res)=>{
       const dirtyScratcherData = await extractScratcherData(lottoURL)
       const dataForDB = cleanScratcherData(dirtyScratcherData)
       console.log(dataForDB);
+      console.log(dirtyScratcherData.length);
       console.log(dataForDB.length);
     }
 
--- mysql.js
+++ mysql.js
@@ -1,6 +1,6 @@
 async function updateDBScratcherData (array){
-  const insertGame = ``
-  const insertScratcher = `INSERT INTO scratcher_data (scrape_date,game_id,prize,odds,remaining_prize,total_prize) VALUES (${},${},${},${},${})`
+  const insertGameData = `INSERT INTO game (name,scratcher_lotto_id,gametype_id,active) VALUES (${},${},${},${})`
+  const insertScratcherData = `INSERT INTO scratcher_data (scrape_date,game_id,prize,odds,remaining_prize,total_prize) VALUES (${},${},${},${},${})`
 
 }
 
