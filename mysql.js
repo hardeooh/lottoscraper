b187fa995fc2f32113e34db4629a8b8e8cae1500
hardeooh <harrydaleoh@gmail.com>
2023-08-03 00:40:14 -0700

    ready for node cron

--- mysql.js
+++ mysql.js
@@ -12,14 +12,13 @@ async function updateScratcherGameData(cleanData){
   // Insert into database games that do not exist yet
   await uniqueGameData.map(e=>{
     const insertGameData = `INSERT INTO game (name,scratcher_lotto_id,gametype_id,active) VALUES ('${e["name"]}',${e["scratcher_id"]},1,1);`;
-
+  
     connection.query(`SELECT name FROM game WHERE name='${e["name"]}';`, (err,rows)=>{  
       if (err){
         throw err;
       }
-        if(rows.length===0){
+      if(rows.length===0){
         connection.query(insertGameData)
-        console.log('insertion into game table done!');   
         }     
       }
     )
@@ -41,8 +40,5 @@ async function updateScratcherRowData(cleanData){
       }
     )})
   }
-
-
-
 
 module.exports = { updateScratcherGameData, updateScratcherRowData }
