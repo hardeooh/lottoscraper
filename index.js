2bc64a733623bb97efd1c82d319f32298bc3508d
hardeooh <harrydaleoh@gmail.com>
2023-08-07 00:51:50 -0700

    Added try/catch blocks as first measure against pages failing to load

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const { getScratcherURL, extractScratcherData, cleanScratcherData } = require('./pullData.js')
 const { updateScratcherGameData, updateScratcherRowData } = require('./mysql.js')
+const cron = require('node-cron');
 const playwright = require('playwright');
 require('dotenv').config()
 const mysql = require('mysql2')
 const express = require('express')
+
 const connection = mysql.createConnection(process.env.DATABASE_URL)
 console.log('Connected to PlanetScale!')
-
 connection.connect()
+
+const url = 'https://www.calottery.com/scratchers#endTable'
 
 const app = new express();
 const port = process.env.port || 3000;
@@ -25,17 +28,19 @@ app.get('/api/gt', (req,res)=>{
 
 
     async function main(){
-      const lottoURL = await getScratcherURL()
+      
+      const lottoURL = await getScratcherURL(url)
       const dirtyScratcherData = await extractScratcherData(lottoURL)
       const dataForDB = cleanScratcherData(dirtyScratcherData)
       await updateScratcherGameData(dataForDB)
       setTimeout(() => {
         updateScratcherRowData(dataForDB)
       }, 5000);
-
-
-      console.log(dirtyScratcherData.length);
-      console.log(dataForDB.length);
+      console.log(dataForDB.length,`total rows without headers`);
     }
+    main();
 
-    main()
+    // cron.schedule('*/8 * * * *', () => {
+    //   console.log('running a task every 10 minutes');
+    //   main();      
+    // });
